05068e64db74d01ffdd6799b7c6e7147faf4ad9a
Ankana Majee <majeeankana4@gmail.com>
2025-10-18 23:51:43 +0530

    connect database

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from models import Product
+from config import SessionLocal, engine
+import database_models
+
 
 app = FastAPI()
+database_models.Base.metadata.create_all(bind=engine)
 
 # @app.get("/")
 # def greet():
@@ -12,10 +16,23 @@ products = [
     Product(id=2, name="laptop", description="gaming laptop",  price=999, quantity=6),
     Product(id=3, name="tablet", description="android tablet",  price=199, quantity=15),
     Product(id=4, name="monitor", description="4k monitor",  price=299, quantity=8),
-]    
+]  
+
+    
+def init_db():
+    db = SessionLocal()
+    count = db.query(database_models.Product).count
+    if count ==0:
+        for product in products:
+            db.add(database_models.Product(**product.model_dump()))    
+        db.commit()     
+        
+init_db()        
     
 @app.get("/products") 
 def get_all_products():
+    # db = SessionLocal()
+    # db.query()
     return  products
 
 @app.get("/product/{id}")
